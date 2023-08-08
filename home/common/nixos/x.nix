ddc492ff080eb4527524dc666d53d93b9a5d4792
Zijun Yu <zijun.yu.joey@gmail.com>
2023-08-08 17:16:01 +0200

    WIP xmonad

---
+++ home/common/nixos/x.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }:
+let
+  polybarOpts = ''
+    # ${pkgs.nitrogen}/bin/nitrogen --restore &
+    ${pkgs.pasystray}/bin/pasystray &
+    ${pkgs.blueman}/bin/blueman-applet &
+    ${pkgs.networkmanagerapplet}/bin/nm-applet --sm-disable --indicator &
+    # megasync &
+  '';
+in
+{
+  home.packages = with pkgs; [
+    dialog                 # Dialog boxes on the terminal (to show key bindings)
+    networkmanager_dmenu   # networkmanager on dmenu
+    networkmanagerapplet   # networkmanager applet
+    nitrogen               # wallpaper manager
+    xcape                  # keymaps modifier
+    xorg.xkbcomp           # keymaps modifier
+    xorg.xmodmap           # keymaps modifier
+    xorg.xrandr            # display manager (X Resize and Rotate protocol)
+    pavucontrol            # pulseaudio volume control
+    paprefs                # pulseaudio preferences
+    pasystray              # pulseaudio systray
+    xdotool                # for rofi-emoji to insert emojis directly
+  ];
+
+  xsession = {
+    enable = true;
+    initExtra = polybarOpts;
+
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = hpkgs: [
+        hpkgs.dbus
+        hpkgs.monad-logger
+        hpkgs.xmonad-contrib
+      ];
+      config = ./xmonad.hs;
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+    terminal = "${pkgs.alacritty}/bin/kitty";
+    # theme = ./theme.rafi;
+  };
+}
--- home/y7000.nix
+++ home/y7000.nix
@@ -45,8 +45,6 @@
     };
   };
 
-  xsession.enable = true;
-
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home = {
